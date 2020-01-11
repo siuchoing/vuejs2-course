161c3ee332aaa334a4851dcdda362a51fe371755
altantic <15200582@life.hlkbu.edu.hk>
2020-01-12 00:22:02 +0800

    Initiate the code in App.vue

--- src/App.vue
+++ src/App.vue
@@ -1,17 +1,8 @@
 <template>
-    <div class="container">
-        <div class="row">
-            <div class="col-xs-12 col-sm-8 col-sm-offset-2 col-md-6 col-md-offset-3">
-                <h1>Components</h1>
-
-            </div>
-        </div>
-    </div>
+    <app-server-status></app-server-status>
 </template>
 
 <script>
-    export default {
-    }
 </script>
 
 <style>
