2295d8dda603f61feb579f25691ed6c20eaef8a4
altantic <15200582@life.hlkbu.edu.hk>
2020-01-12 14:22:46 +0800

    reverse the name by method

--- src/components/UserDetail.vue
+++ src/components/UserDetail.vue
@@ -1,13 +1,18 @@
 <template>
     <div class="component">
         <h3>You may view the User Details here</h3>
-        <p>Many Details {{ myName }}</p>
+        <p>Many Details {{ switchName() }}</p>
     </div>
 </template>
 
 <script>
     export default {
-        props: ['myName']
+        props: ['myName'],
+        methods: {
+            switchName() {
+                return this.myName.split("").reverse().join("");
+            }
+        }
     }
 </script>
 
