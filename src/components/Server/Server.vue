6880eea64fe5ddc62e36dfad016779cd99842dbc
altantic <15200582@life.hlkbu.edu.hk>
2020-01-13 23:42:45 +0800

    Create Server component

---
+++ src/components/Server/Server.vue
@@ -0,0 +1,18 @@
+<template>
+    <li
+            class="list-group-item"
+            style="cursor: pointer">
+        Server #{{ id }}
+    </li>
+</template>
+
+<script>
+    export default {
+        // passing id from Servers.vue to Server.vue by props [parent to child]
+        props: ['id'],
+    }
+</script>
+
+<style>
+
+</style>
